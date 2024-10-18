07e488fa59111d44be14a09f8ce037d124c0d413
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2024-10-18 12:57:09 +0200

    add doc strings

--- sctools/annotate.py
+++ sctools/annotate.py
@@ -4,6 +4,21 @@ import numpy as np
 
 
 def majority_vote(adata, prediction_col, clustering_resolution, majority_col_name = None):
+    '''
+    performs a majority annotation of cells based on their neighbourhood. In brief, the function
+    performs Leiden clustering with `clustering_resolution` resolution and then annotates all cells
+    in a given cluster with the annotation of the majority of the cells in the cluster. The reasoning
+    behind this is that cells that are in close proximity are also similar. This implementation
+    was adapted from the celltypist package.
+
+    :param adata:                     AnnData object to perform annotation on. Has to have a computed kNN-graph (see sc.pp.neighbors)
+    :param prediction_col:            string denoting the column to compute the majority annotation from              
+    :param clustering_resolution:     resolution of the leiden clustering to use for majority annotation
+    :param majority_col_name:         optional name of the new majority annotation column
+
+    :return:                          None
+    '''
+    
     # partly taken from celltypist
     key_added = f'leiden_scvi_{clustering_resolution}'
     sc.tl.leiden(
@@ -30,6 +45,18 @@ def majority_vote(adata, prediction_col, clustering_resolution, majority_col_nam
 
 
 def annotate_adata_on_gene_hi_lo(adata, gene, resolution = 5):
+    '''
+    annotates cells based on the expression of a given gene in hi and lo expressing cells
+    based on the median expression (i.e. gene expression of cell < median = lo else high)
+    The raw annotation is then propagated to neighboring cells using a majority voting
+    to mitigate possible dropouts
+
+    :param adata:        AnnData object to perform the annotation for (needs to have a kNN-graph computed)
+    :param gene:         name of the gene to annotate cells from
+    :param resolution:   resolution of the leiden clustering used for majority propagation
+
+    :return:             None
+    '''
     expression_values = adata[:, gene].X.toarray().flatten()
     threshold = np.median(expression_values)
     status_column = f'{gene.lower()}_status'
