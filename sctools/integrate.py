105723e31c0c6c098f8bfd71d2114f27e32d3f04
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2024-10-18 13:31:18 +0200

    add docstrings

--- sctools/integrate.py
+++ sctools/integrate.py
@@ -2,7 +2,17 @@ import scvi
 
 import scanpy as sc
 
+
 def filter_too_few_cell_batches(adata, batch_key, min_cells = 5):
+    '''
+    computes the number of cells in each batch given in batch_key of adata.obs
+    and removes all batches with fewer than `min_cells' cells. This function is mainly used
+    for ensuring enough samples per batch for scVI.
+
+    :param adata:      AnnData to filter
+    :param batch_key:  column in adata.obs to use for counting the cells
+    :param min_cells:  minimum number of cells per batch (default: 5)
+    '''
     cells_per_batch = adata.obs.groupby(batch_key).count().iloc[:, 0]
     print(
         f'filtered the following batches < {min_cells} cells:\n', 
@@ -25,6 +35,21 @@ def integrate_data_scvi(
     **kwargs
     
 ):
+    '''
+    integrate data using scVI based on batch_key and computes a umap on the computed latent representation. 
+    This is basically set up as described in the scIB package https://github.com/theislab/scib/blob/main/scib/integration.py. 
+
+    :param adata:                        AnnData object to perform the integration on
+    :param batch_key:                    string denoting the column containing the batch information (e.g. sample/patient ID)
+    :param categorical_covariate_keys:   list of strings denoting categorical covariates to consider for scVI (i.e. remove the effect of)
+    :param continuous_covariate_keys:    list of strings denoting continuous covariates to consider for scVI (i.e. remove the effect of)
+    :param use_highly_variable_genes:    whether to use all genes present or just the highly variable genes (default: True)
+    :param n_top_genes:                  if to use highly variable genes how many to compute
+    :param filter_small_batches:         whether to filter out small batches (i.e. batches with less then 5 cells)
+    :param **kwargs:                     keyword arguments to pass to scVI.train method
+
+    :return:                             dictionary with keys 'data' and 'model' containing the integrated data object as well as the trained scVI model
+    '''
     if filter_small_batches:
         adata = filter_too_few_cell_batches(adata, batch_key)
 
