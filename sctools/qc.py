eb8c5889fd58ec56400d8733f2156cdc537fa873
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2024-10-18 13:52:06 +0200

    add docstrings

--- sctools/qc.py
+++ sctools/qc.py
@@ -41,21 +41,48 @@ import numpy as np
 #     return doublet_frame, int(doublet.sum()), len(doublet)
 
 
-def compute_qc_metrics(adata):
+def get_nexpressed(adata):
+    '''
+    computes number of expressed genes per cell
+    '''
+    return np.array((adata.X > 0).sum(axis = 0)).flatten()
+
+
+def compute_qc_metrics(adata, mitochondrial_re = '^MT.', ribosomal_re = '^RP[SL]'):
+    '''
+    computes cell quality metrics like number of expressed features, total UMI count, percent mitochondrial UMIs
+    and percent ribosomal UMIs.
+
+    :param adata:               AnnData object to compute cell QC metrics for
+    :param mitochondrial_re:    regular expression pattern to use for identification of mitochondrial genes
+    :param ribosomal_re:        regular expression pattern to use for identification of ribosomal genes
+
+    :return:                    None
+    '''
     # flatten is needed due to csr_matrix.sum returning a numpy.matrix object
     # which cannot be broadcasted to obs frame
-    adata.obs['nFeature_RNA'] = np.array((adata.X > 0).sum(axis = 1)).flatten()
+    adata.obs['nFeature_RNA'] = get_nexpressed(adata)
     adata.obs['nCount_RNA'] = np.array(adata.X.sum(axis = 1)).flatten()
     adata.obs['percent_mt'] = np.array(
-        adata[:, adata.var.index.str.match('^MT.')].X.sum(axis = 1) / adata.X.sum(axis = 1) * 100
+        adata[:, adata.var.index.str.match(mitochondrial_re)].X.sum(axis = 1) / adata.X.sum(axis = 1) * 100
     ).flatten()
 
     adata.obs['percent_ribo'] = np.array(
-        adata[:, adata.var.index.str.match('^RP[SL]')].X.sum(axis = 1) / adata.X.sum(axis = 1) * 100
+        adata[:, adata.var.index.str.match(ribosomal_re)].X.sum(axis = 1) / adata.X.sum(axis = 1) * 100
     ).flatten()
 
 
 def apply_qc_thresholds(adata, sample_id_column, sample_thresholds):
+    '''
+    takes a dictionary of thresholds per sample id and assesses whether a given cell passed all thresholds
+    which is reported in the 'qc_pass' column added to adata.obs dataframe
+
+    :param adata:               AnnData object to assess QC passing on
+    :param sample_id_column:    string denoting column containing sampleIDs to use for indexing 'sample_threshold' dictionary
+    :param sample_threshold:    dictionary of dictionaries containing qc_metric: (lo, hi) thresholds per sampleID in 'sample_id_column'
+
+    :return:                    None
+    '''
     adata.obs['qc_pass'] = True
     for sample_id, thresholds in sample_thresholds.items():
         df = adata.obs.loc[adata.obs[sample_id_column] == sample_id, :]
@@ -70,7 +97,3 @@ def apply_qc_thresholds(adata, sample_id_column, sample_thresholds):
             axis = 0
         )
         adata.obs.loc[adata.obs[sample_id_column] == sample_id, 'qc_pass'] = qc_pass
-
-
-def get_nexpressed(adata):
-    return np.array((adata.X > 0).sum(axis = 0)).flatten()
