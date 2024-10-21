7e3f72d924176ff815b043f768002f4c9c31116d
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2024-10-21 09:41:48 +0200

    add examples

--- sctools/qc.py
+++ sctools/qc.py
@@ -58,6 +58,14 @@ def compute_qc_metrics(adata, mitochondrial_re = '^MT.', ribosomal_re = '^RP[SL]
     :param ribosomal_re:        regular expression pattern to use for identification of ribosomal genes
 
     :return:                    None
+
+    :Usage:
+    ```
+    from sctools import qc
+
+    # by default this assumes human data so you may want to tune the regular expressions towards your model organism
+    qc.compute_qc_metrics(adata)
+    ```
     '''
     # flatten is needed due to csr_matrix.sum returning a numpy.matrix object
     # which cannot be broadcasted to obs frame
@@ -75,13 +83,44 @@ def compute_qc_metrics(adata, mitochondrial_re = '^MT.', ribosomal_re = '^RP[SL]
 def apply_qc_thresholds(adata, sample_id_column, sample_thresholds):
     '''
     takes a dictionary of thresholds per sample id and assesses whether a given cell passed all thresholds
-    which is reported in the 'qc_pass' column added to adata.obs dataframe
+    which is reported in the 'qc_pass' column added to adata.obs dataframe (requires compute_qc_metrics to be run first)
 
     :param adata:               AnnData object to assess QC passing on
     :param sample_id_column:    string denoting column containing sampleIDs to use for indexing 'sample_threshold' dictionary
     :param sample_threshold:    dictionary of dictionaries containing qc_metric: (lo, hi) thresholds per sampleID in 'sample_id_column'
 
     :return:                    None
+
+    :Usage:
+    ```
+    from sctools import qc
+    
+    MIN_RNA_FEATURES = 750
+    MAX_RNA_FEATURES = 6000
+    MAX_PERCENT_MT = 15
+
+    # define filtering thresholds
+    qc_thresholds = {
+        k: {
+            'nFeature_RNA': (MIN_RNA_FEATURES, MAX_RNA_FEATURES), 
+            'percent_mt': (0, MAX_PERCENT_MT), 
+            'percent_ribo': (5, 100)
+        } 
+        for k 
+        in adata.obs.sample_id
+    }
+
+    # compute QC passing cells
+    # this requires compute_qc_metrics to be run first
+    qc.apply_qc_thresholds(
+        adata, 
+        'sample_id', 
+        qc_thresholds
+    )
+
+    # filter data
+    adata = adata[adata.obs.qc_pass, :].copy()
+    ```
     '''
     adata.obs['qc_pass'] = True
     for sample_id, thresholds in sample_thresholds.items():
